68c3bba3ccd31bf8ff48858fda099328ff1164d5
jianjin.wu <mosaic101@foxmail.com>
2017-12-21 15:18:28 +0800

    add qcloud api config

--- src/lib/mqtt.js
+++ src/lib/mqtt.js
@@ -6,28 +6,39 @@
 /*   By: JianJin Wu <mosaic101@foxmail.com>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/12/01 15:13:34 by JianJin Wu        #+#    #+#             */
-/*   Updated: 2017/12/15 11:28:42 by JianJin Wu       ###   ########.fr       */
+/*   Updated: 2017/12/21 15:18:00 by JianJin Wu       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 const debug = require('debug')('mqtt')
 const mqtt = require('mqtt')
+const crypto = require('crypto')
 const config = require('getconfig')
 
+// qcloud api config
+const {appid, secretId, secretKey} = config.qcloud
+const srcStr = `Appid=${appid}&Instanceid=mqtt-5p50b9y7s&Action=Connect`
+
+const hmac = crypto.createHmac('sha256', secretKey)
+const password = hmac.update(srcStr).digest('base64')
+
 const MQTT_URL = `mqtt://${config.mqtt.host}:${config.mqtt.port}`
+
 const settings = {
-  clientId: 'cloud_' + Math.random().toString(16).substr(2, 8),
+  clientId: 'mqtt-5p50b9y7s@cloud_' + Math.random().toString(16).substr(2, 8),
   clean: true,
   keepalive: 3,
   reconnectPeriod: 5 * 1000,
-  connectTimeout: 10 * 1000
+  connectTimeout: 10 * 1000,
+  username: secretId,
+  password: password
 }
 const client = mqtt.connect(MQTT_URL, settings)
 
 // subcribe topic
-client.subscribe(`$queue/station/connect`, { qos: 1 })
-client.subscribe(`$queue/station/disconnect`, { qos: 1 })
+client.subscribe(`station/connect`, { qos: 1 })
+client.subscribe(`station/disconnect`, { qos: 1 })
 
 // connect
 client.on('connect', connack => debug('cloud connect successfully!', connack))
